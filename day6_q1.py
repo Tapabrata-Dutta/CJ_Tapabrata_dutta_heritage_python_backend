3db3709b6e2ae15cc5cd808645bc0afd8b8400d9
TAPABRATA DUTTA <tapabratadutta777@gmail.com>
2026-06-10 09:36:59 +0530

    Done day 6 assignment

---
+++ day6_q1.py
@@ -0,0 +1,9 @@
+
+# code runs when condition is True
+
+
+# Example
+temperature = 38
+if temperature > 37:
+    print('You have a fever!')
+
---
+++ day6_q5.py
@@ -0,0 +1,6 @@
+x = 3
+if x != 0 and (10 / x) > 1:
+    print('Result is greater than 1')
+else:
+    print('Safe — no crash!')
+
